2f5d028f99a065db27c38e8a89b63603c3c87fe2
hzouitine <3zouitine@gmail.com>
2018-05-17 11:10:06 +0000

    Add User & Role domains

---
+++ src/main/java/com/springbootsecurity/demo/domain/Role.java
@@ -0,0 +1,33 @@
+package com.springbootsecurity.demo.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String role;
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+
+
+}
---
+++ src/main/java/com/springbootsecurity/demo/domain/User.java
@@ -0,0 +1,58 @@
+package com.springbootsecurity.demo.domain;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    private String fullName;
+
+    @ManyToMany
+    @JoinTable(joinColumns = {@JoinColumn(name = "user_id")}, inverseJoinColumns = {@JoinColumn(name = "role_id")})
+    private List<Role> roles;
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+
+}
